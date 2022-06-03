23541527563aca7ab27ad2b65f6bc6015439e4f2
BK Venkata UdayKumar <31610853+uday160386@users.noreply.github.com>
2022-06-03 18:45:40 +0800

    updated file name

--- src/test/java/com/swotitup/secops/CustomerRepositoryTest.java
+++ src/test/java/com/swotitup/secops/CustomerRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.swotitup.secops;
 
 import com.swotitup.secops.model.Customers;
-import org.junit.Test;
+
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
